7246ea08bedc5e3652906433c4157a618a5ede8a
Jonathan Choi <jonchoi11@gmail.com>
2021-11-05 20:17:52 -0600

    finish intro

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello, I'm Jonathan Choi. <br /> Welcome to My Personal Portfolio!
+      </SectionTitle>
+      <SectionText>
+        I'm a full-stack developer and recent graduate from Lighthouse Labs. I'm
+        currently seeking new opportuniites and am eager to continue my growth
+        as a developer.
+      </SectionText>
+      <Button onClick={() => (window.location = "https://google.ca")}>
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
