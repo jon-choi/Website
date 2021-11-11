848e23ba443fe8d108c46d74cac8c189c6abd7c1
Jonathan Choi <jonchoi11@gmail.com>
2021-11-11 14:34:34 -0700

    add navdropdown component

--- src/components/NavDropDown/index.js
+++ src/components/NavDropDown/index.js
@@ -1,13 +1,49 @@
-import React from 'react'
-import { AiFillPhone, AiOutlineMail } from 'react-icons/ai'
-import { FaLocationArrow } from "react-icons/fa"
+import React from "react";
+import { AiFillPhone, AiOutlineMail } from "react-icons/ai";
+import { FaLocationArrow } from "react-icons/fa";
 
-import { DropDownContainer, DropDownIcon, DropDownItem, DropDownItemDesc, DropDownItemTitle, DropDownTextContainer } from './NavDropDown'
+import {
+  DropDownContainer,
+  DropDownIcon,
+  DropDownItem,
+  DropDownItemDesc,
+  DropDownItemTitle,
+  DropDownTextContainer,
+} from "./NavDropDown";
 
 const NavDropDown = (props) => (
-  <div>
-    NavDropDown
-  </div>
+  <DropDownContainer active={props.isOpen}>
+    <DropDownItem href="#" target="_blank" rel="noreferrer">
+      <DropDownIcon>
+        <AiFillPhone />
+      </DropDownIcon>
+      <DropDownTextContainer>
+        <DropDownItemTitle>Phone</DropDownItemTitle>
+        <DropDownItemDesc>Let's have a quick chat!</DropDownItemDesc>
+      </DropDownTextContainer>
+    </DropDownItem>
+    <DropDownItem href="#" target="_blank" rel="noreferrer">
+      <DropDownIcon>
+        <AiOutlineMail />
+      </DropDownIcon>
+      <DropDownTextContainer>
+        <DropDownItemTitle>Email</DropDownItemTitle>
+        <DropDownItemDesc>
+          Feel free to send me an email and I'll get back to you as soon as I
+          can!
+        </DropDownItemDesc>
+      </DropDownTextContainer>
+    </DropDownItem>
+    <DropDownItem href="#" target="_blank" rel="noreferrer">
+      <DropDownIcon>
+        <FaLocationArrow />
+      </DropDownIcon>
+      <DropDownTextContainer>
+        <DropDownItemTitle>City</DropDownItemTitle>
+        <DropDownItemDesc>Calgary, AB</DropDownItemDesc>
+      </DropDownTextContainer>
+    </DropDownItem>
+  </DropDownContainer>
 );
 
-export default NavDropDown
+export default NavDropDown;
